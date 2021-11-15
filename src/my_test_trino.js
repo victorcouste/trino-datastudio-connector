7146e52fb0af4e6fbeb3ad00c601b159b646a057
Victor Coustenoble <victor.coustenoble@gmail.com>
2021-11-15 08:58:47 +0100

    Init Trino Data Studio connector project

--- src/my_test_trino.js
+++ src/my_test_trino.js
@@ -1,8 +1,12 @@
 function test_trino() {
 
-  var url="http://trino_url:8080";
-  var user="xxxxx";
-  var password="";
+  //var url="http://34.107.95.186:8080";
+  //var user="starburst_service";
+  //var password="";
+
+  var url="https://sbe-official-demo-azure.fieldeng.starburstdata.net";
+  var user="sa.victor.coustenobl";
+  var password="P@$$w0rd06";
   var catalog="tpch";
   var schema="tiny";
   var object="nation";
